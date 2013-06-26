090fdf92cb9aca3130d92d4d944914a082d966ee
Borna Novak <dosadnizub@gmail.com>
2013-06-26 13:52:56 +0200

    An attempt to fix rails 3.0 support

--- lib/simple_form_class/owner_validator.rb
+++ lib/simple_form_class/owner_validator.rb
@@ -4,8 +4,12 @@ module SimpleFormClass
     def validate(record)
       owner_sym = options[:owner]
       owner = record.send(owner_sym)
+      
+      # Rails 3.0 have error_messages, Rails > 3.0 have errors.messages
+      error_messages = ::Rails.version < "3.1" ? owner.errors.full_messages :
+        owner.errors.messages
 
-      unless owner.errors.messages.empty?
+      unless error_messages.empty?
         record.errors.add(owner_sym, "is invalid due to #{owner.errors.messages}")
       end
     end
